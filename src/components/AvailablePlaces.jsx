438698f26a4bcd771f7055bf145cda98d07c946d
Elvan Hidayat <elvanhd@gmail.com>
2023-12-09 23:14:12 +0700

    extracting http code

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 
+import { fetchAvailablePlaces } from '../http.js';
+import { sortPlacesByDistance } from '../loc.js';
 import Error from './Error.jsx';
 import Places from './Places.jsx';
 
@@ -13,22 +15,24 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch('http://localhost:3000/places');
-        const resData = await response.json();
+        const places = await fetchAvailablePlaces();
 
-        if (!response.ok) {
-          throw new Error('Failed to fetch places');
-        }
-
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         setError({
           message:
             error.message || 'Could not fetch places, please try again later.',
         });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
 
     fetchPlaces();
