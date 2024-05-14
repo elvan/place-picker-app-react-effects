87c5ffef94a76cd95c070ea00068f6cf4a546212
Elvan Hidayat <elvanhd@gmail.com>
2024-05-14 20:17:13 +0700

    Optimize handleRemovePlace with useCallback
    - Import useCallback in App.jsx
    - Wrap handleRemovePlace with useCallback for optimization
    - Pass handleRemovePlace as a dependency in DeleteConfirmation's useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import logoImg from './assets/logo.png';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import Modal from './components/Modal.jsx';
@@ -51,7 +51,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -62,7 +62,7 @@ function App() {
       'selectedPlaces',
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
     );
-  }
+  }, []);
 
   return (
     <>
